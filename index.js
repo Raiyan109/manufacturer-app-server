ade9d871ecb5eec8e474237f4578900139e49735
Raiyan109 <kraiyan109@gmail.com>
2023-03-22 10:41:59 +0600

    get

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
 app.use('/api/parts', partRoutes)
 app.use('/api/reviews', reviewRoutes)
 app.use('/api/testimonials', testimonialRoutes)
